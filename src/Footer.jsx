5b2f372fe96235b8a3f4609ecc17bb40080fb43e
suvaranjan <suvaranjan01@gmail.com>
2023-10-01 10:28:47 +0530

    update css

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -24,7 +24,7 @@ export default function Footer() {
           style={
             isDarkMode
               ? {
-                  color: "#8324ff",
+                  color: "#bdabfb",
                 }
               : null
           }
