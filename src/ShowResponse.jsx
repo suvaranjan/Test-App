ed9882d4b1c396bb97b49d29d1edc1fb80609958
suvaranjan <suvaranjan01@gmail.com>
2023-10-11 20:02:53 +0530

    changes css

--- src/ShowResponse.jsx
+++ src/ShowResponse.jsx
@@ -28,13 +28,34 @@ export default function ShowResponse() {
             ? "Internet of Things"
             : "Cyber Security and Privacy"}
         </h1>
-        <p>{week === "all" ? "All Weeks" : week.replace(/(\d+)/, " $1")}</p>
+        <p
+          style={
+            isDarkMode
+              ? {
+                  color: "#cfcfcf",
+                }
+              : null
+          }
+        >
+          {week === "all" ? "All Weeks" : week.replace(/(\d+)/, " $1")}
+        </p>
       </div>
       {Userresponse > "" ? (
         <div className="resBox2">
           {Userresponse.map((response, index) => (
             <div className="userResponses" key={index}>
-              <div className="question">
+              <div
+                className="question"
+                style={
+                  isDarkMode
+                    ? {
+                        color: "#d0d0d0",
+                      }
+                    : {
+                        color: "#000",
+                      }
+                }
+              >
                 {index + 1} {". "}
                 {response.question}
               </div>
@@ -53,7 +74,19 @@ export default function ShowResponse() {
                     }
                   `}
                     key={optionKey}
-                    style={isDarkMode ? { border: "1px solid #4a4a4f" } : null}
+                    style={
+                      !isDarkMode &&
+                      response.options[optionKey] === response.answer
+                        ? {
+                            background: "#018786",
+                            color: "#fff",
+                            // fontWeight: "600",
+                            border: "1px solid #4a4a4f",
+                          }
+                        : isDarkMode
+                        ? { border: "1px solid #4a4a4f" }
+                        : null
+                    }
                   >
                     {`${optionKey}. ${response.options[optionKey]}`}
                   </div>
